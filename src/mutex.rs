b9c319fe42a486932ba3a74456959675f1d87a16
no2ca <taiseiono6@gmail.com>
2026-03-04 12:50:40 +0900

    feat: Implement Mutex and MutexGuard with spin lock

---
+++ src/mutex.rs
@@ -0,0 +1,73 @@
+use core::{
+    cell::UnsafeCell,
+    marker::PhantomData,
+    ops::{Deref, DerefMut},
+    sync::atomic::{AtomicBool, Ordering},
+};
+
+pub struct Mutex<T: ?Sized> {
+    lock: AtomicBool,
+    data: UnsafeCell<T>,
+}
+
+pub struct MutexGuard<'a, T: ?Sized + 'a> {
+    lock: &'a AtomicBool,
+    data: &'a mut T,
+    // 獲得した値がスレッド間で共有されないようにする
+    // 否定トレイトが完全実装されたら以下を使用できる
+    // impl<T: ?Sized> !Send for MutexGuard<'_, T> {}
+    _forbid_send: PhantomData<*const ()>,
+}
+
+impl<T> Mutex<T> {
+    pub const fn new(d: T) -> Mutex<T> {
+        Mutex {
+            lock: AtomicBool::new(false),
+            data: UnsafeCell::new(d),
+        }
+    }
+}
+
+impl<T: ?Sized> Mutex<T> {
+    pub fn lock(&self) -> MutexGuard<'_, T> {
+        loop {
+            while self.lock.load(Ordering::Relaxed) {
+                core::hint::spin_loop();
+            }
+            if self
+                .lock
+                .compare_exchange(false, true, Ordering::Acquire, Ordering::Relaxed)
+                .is_ok()
+            {
+                return MutexGuard {
+                    lock: &self.lock,
+                    data: unsafe { &mut *self.data.get() },
+                    _forbid_send: PhantomData,
+                };
+            }
+        }
+    }
+}
+
+unsafe impl<T: ?Sized + Send> Send for Mutex<T> {}
+unsafe impl<T: ?Sized + Send> Sync for Mutex<T> {}
+unsafe impl<T: ?Sized + Sync> Sync for MutexGuard<'_, T> {}
+
+impl<T: ?Sized> Deref for MutexGuard<'_, T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &*self.data
+    }
+}
+
+impl<T: ?Sized> DerefMut for MutexGuard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut *self.data
+    }
+}
+
+impl<T: ?Sized> Drop for MutexGuard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.store(false, Ordering::Release);
+    }
+}
